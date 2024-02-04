ac1722c5c302c9e700d56f6e10c35a5ca72e00f2
RushabhBhosale <145191870+RushabhBhosale@users.noreply.github.com>
2024-02-04 12:45:17 +0530

    updates

---
+++ my-watchlist/components/CardList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+const CardList = ({popularMovie}) => {
+   return (
+      <div className="mt-5 popular list overflow-x-auto">
+         <div className="flex gap-5">
+            {popularMovie && (popularMovie.results ? popularMovie.results : popularMovie.items).map((movie, index) => (
+               <MovieCard movie={movie} key={index} />
+            ))}
+         </div>
+      </div>
+   )
+}
+
+export default CardList
---
+++ my-watchlist/components/Sidebar.jsx
@@ -0,0 +1,48 @@
+'use client'
+import { ak } from '@/utils/api'
+import axios from 'axios'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+const Sidebar = () => {
+
+  const [genre, setGenre] = useState()
+
+  useEffect(() => {
+    fetchGenres()
+  }, [])
+
+  const fetchGenres = async () => {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/genre/list?${ak}`);
+      setGenre(response.data);
+    } catch (error) {
+      console.error("Error fetching TV data:", error);
+    }
+  };
+
+  return (
+    <div className='ps-8 h-[38rem] overflow-auto'>
+      <p className='font-bold p-2 my-2'>GENRE</p>
+      {genre && genre.genres.map((gen, index) => (
+        <p className='text-gray-600 hover:text-gray-400 p-2 cursor-pointer' key={index}>{gen.name}</p>
+      ))}
+      <p className='font-bold p-2 my-2'>Languages</p>
+      <p className='text-gray-600 hover:text-gray-400 p-2 cursor-pointer'>Marathi</p>
+      <p className='text-gray-600 hover:text-gray-400 p-2 cursor-pointer'>Hindi</p>
+      <p className='text-gray-600 hover:text-gray-400 p-2 cursor-pointer'>Japanese</p>
+      <p className='text-gray-600 hover:text-gray-400 p-2 cursor-pointer'>English</p>
+      <p className='text-gray-600 hover:text-gray-400 p-2 cursor-pointer'>Korean</p>
+      <p className='text-gray-600 hover:text-gray-400 p-2 cursor-pointer'>English</p>
+
+      <div className='p-2'>
+        <Image src='/tmdb-logo.png' className='py-3' width={150} height={40} alt='tmdblogo' />
+        <p className='text-gray-500'>Copyright 2024</p>
+        <p className='text-gray-500'>Rushabh Bhosale</p>
+      </div>
+    </div>
+  )
+
+}
+
+export default Sidebar
